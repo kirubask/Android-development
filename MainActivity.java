f769fa57dcd669d0cf430802473bdd1af5976348
kirubask <kirubakaran08278@gmail.com>
2022-10-15 15:54:00 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void display(View view) {
+        Toast.makeText(getApplicationContext(), "hai it's me kiruba", Toast.LENGTH_SHORT).show();
+    }
+}
